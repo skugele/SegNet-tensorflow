f6d671b6871e11bd15be69e0e9bf935b9c89c737
skugele <11bwai$mmim1>
2018-11-30 15:48:26 -0600

    Add method to calculate "error map" between predictions and ground truth.  Add plotting to images, ground truths, predictions, and error maps.

--- eval_segnet.py
+++ eval_segnet.py
@@ -2,9 +2,13 @@ import collections
 import random
 import numpy as np
 import tensorflow as tf
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib import gridspec
 
 from sklearn.metrics import confusion_matrix
-from drawings_object import writeImage, display_image
+from drawings_object import writeImage, display_image, background, cylinder, cube, sphere
 from inputs_object import get_filename_list, get_all_test_data
 from SegNet import SegNet
 
@@ -114,11 +118,79 @@ def get_stats_per_image(indices, images, labels, predictions, n_classes):
     return stats
 
 
+def get_diff(prediction, ground_truth):
+    error_color = [200, 0, 0]
+    diff_image = np.zeros((ground_truth.shape[0], ground_truth.shape[1], 3))
+    diff_image[:, :] = [0, 200, 0]
+    diff_image[prediction != ground_truth] = error_color
+
+    return Image.fromarray(np.uint8(diff_image))
+
+
+def display_results(plot_set):
+    nrows = plot_set.size
+    ncols = 4
+
+    fig = plt.figure()
+
+    spec = gridspec.GridSpec(nrows, ncols)
+    spec.update(wspace=0.05, hspace=0.05)  # set the spacing between axes.
+
+    for row in range(nrows):
+        offset = row * ncols
+
+        ax1 = fig.add_subplot(spec[row, 0])
+        if row == 0:
+            ax1.set_title('Image')
+
+        imgplot = plt.imshow(plot_set.images[row])
+        plt.xticks([])
+        plt.yticks([])
+
+        # plt.colorbar(ticks=[0.1, 0.3, 0.5, 0.7], orientation='horizontal')
+
+        ax2 = fig.add_subplot(spec[row, 1], sharex=ax1)
+        if row == 0:
+            ax2.set_title('Ground Truth')
+
+        imgplot = plt.imshow(plot_set.ground_truths[row])
+        plt.xticks([])
+        plt.yticks([])
+
+        ax3 = fig.add_subplot(spec[row, 2], sharex=ax1)
+        if row == 0:
+            ax3.set_title('Prediction')
+
+        imgplot = plt.imshow(plot_set.predictions[row])
+        plt.xticks([])
+        plt.yticks([])
+
+        ax4 = fig.add_subplot(spec[row, 3], sharex=ax1)
+        if row == 0:
+            ax4.set_title('Error Map')
+
+        imgplot = plt.imshow(plot_set.error_map[row])
+        plt.xticks([])
+        plt.yticks([])
+
+    # plt.subplots_adjust(wspace=0, hspace=0)
+
+    # labels = ['background', 'cylinder', 'cube', 'sphere']
+    # patches = [mpatches.Patch(color=np.asarray(background) / 256),
+    #            mpatches.Patch(color=np.asarray(cylinder) / 256),
+    #            mpatches.Patch(color=np.asarray(cube) / 256),
+    #            mpatches.Patch(color=np.asarray(sphere) / 256), ]
+    #
+    # fig.legend(labels=labels, handles=patches, ncol=2, loc=2)
+
+    plt.show()
+
+
 def main(argv=None):
     n_examples = None
     indices = None
     # indices = [86, 87, 93, 99]
-    indices, images, labels = load_data(TRAIN_MANIFEST, indices=indices, n=n_examples)
+    indices, images, labels = load_data(VALIDATE_MANIFEST, indices=indices, n=n_examples)
     segnet = SegNet()
 
     predictions = segnet.predict(images, 'model.ckpt-4500')
@@ -140,22 +212,45 @@ def main(argv=None):
     stats_per_image = get_stats_per_image(indices, images, labels, predictions, segnet.n_classes)
 
     # Filter out background images
-    stats_per_image_minus_background_only = filter(lambda stats: sum(map(lambda s: np.ceil(s),stats.f1_scores)) > 1, stats_per_image)
-    stats_per_multi_object_images = filter(lambda stats: sum(map(lambda s: np.ceil(s),stats.f1_scores)) > 2, stats_per_image)
+    stats_per_image_minus_background_only = filter(lambda stats: sum(map(lambda s: np.ceil(s), stats.f1_scores)) > 1,
+                                                   stats_per_image)
+    stats_per_multi_object_images = filter(lambda stats: sum(map(lambda s: np.ceil(s), stats.f1_scores)) > 2,
+                                           stats_per_image)
 
-    best = sorted(stats_per_multi_object_images, key=lambda stats: stats.accuracy, reverse=True)[0:10]
-    for stats in best:
-        display_image(images[stats.index])
-        display_image(writeImage(labels[stats.index], plot=False))
-        display_image(writeImage(predictions[stats.index], plot=False))
+    PlotSet = collections.namedtuple('PlotSet', ['size', 'images', 'ground_truths', 'predictions', 'error_map'])
+
+    best = sorted(stats_per_multi_object_images, key=lambda stats: stats.accuracy, reverse=True)[0:5]
+    best_set = PlotSet(len(best), [], [], [], [])
+
+    for index in map(lambda stats: stats.index, best):
+        best_set.images.append(images[index])
+        best_set.ground_truths.append(writeImage(labels[index], plot=False))
+        best_set.predictions.append(writeImage(predictions[index], plot=False))
+        best_set.error_map.append(get_diff(predictions[index], labels[index]))
 
     worst = sorted(stats_per_image_minus_background_only, key=lambda stats: stats.accuracy)[0:5]
-    for stats in worst:
-        display_image(images[stats.index])
-        display_image(writeImage(labels[stats.index], plot=False))
-        display_image(writeImage(predictions[stats.index], plot=False))
+    worst_set = PlotSet(len(worst), [], [], [], [])
 
+    for index in map(lambda stats: stats.index, worst):
+        worst_set.images.append(images[index])
+        worst_set.ground_truths.append(writeImage(labels[index], plot=False))
+        worst_set.predictions.append(writeImage(predictions[index], plot=False))
+        worst_set.error_map.append(get_diff(predictions[index], labels[index]))
 
+    display_results(best_set)
+    display_results(worst_set)
+
+    # images, ground_truths, predictions, diffs = [], [], [], []
+    # worst = sorted(stats_per_image_minus_background_only, key=lambda stats: stats.accuracy)[0:5]
+    #
+    # worst_indices = map(lambda stats: stats.index, worst)
+    #
+    #
+    # for stats in worst:
+    #     display_image(images[stats.index])
+    #     display_image(writeImage(labels[stats.index], plot=False))
+    #     display_image(writeImage(predictions[stats.index], plot=False))
+    #     display_image(get_diff(predictions[stats.index], labels[stats.index]))
 
 
 if __name__ == '__main__':
